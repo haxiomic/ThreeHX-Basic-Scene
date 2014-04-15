e69aaec11c186d7d5c403b8aa0a68d7f48a91fa7
Brendon Smith <brendonsmith@seacloud9.org>
2014-04-14 19:29:28 -0700

    minor mathutils changes

--- threehx/three/math/MathUtils.hx
+++ threehx/three/math/MathUtils.hx
@@ -1,111 +1,109 @@
-//R66
-/*
-Changed from Math to MathUtils to avoid conflict
-*/
+//r66
+//!# todo possibly rename
 package three.math;
 
-class MathUtils{
-	public static var PI2:Float = Math.PI*2;
+/**
+ * ...
+ * @author Krtolica Vujadin
+ */
+class MathUtils {
 
-	static var chars:Array<String> = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-	public static inline function generateUUID():String{
-		var uuid = new Array<String>();
-		var rnd:Int = 0;
-		var r:Int;
-
-		for (i in 0...36) {
-
-			if ( i == 8 || i == 13 || i == 18 || i == 23 ) {
+	public static var PI2:Float = Math.PI * 2;
+	public static var degreeToRadiansFactor:Float = Math.PI / 180;
+	public static var radianToDegreesFactor:Float = 180 / Math.PI;
+	
+	public function new() {
 		
-				uuid[ i ] = '-';
-		
-			} else if ( i == 14 ) {
-		
-				uuid[ i ] = '4';
-		
-			} else {
-		
-				if (rnd <= 0x02) rnd = 0x2000000 + Std.int((Math.random()*0x1000000))|0;
-				r = rnd & 0xf;
-				rnd = rnd >> 4;
-				uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
-
-			}
+	}
+	
+	public static function generateUUID(?charactersToUse = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"):String {
+		function randomBetween(from:Int, to:Int):Int {
+			return from + Math.floor(((to - from + 1) * Math.random()));
 		}
 		
-		return uuid.join('');
+		var str = "";
+		
+		for (i in 0...36) {
+			if (i == 8 || i == 13 || i == 18 || i == 23) {		
+				str += '-';		
+			} else if (i == 14) {		
+				str += '4';		
+			} else {		
+				str += charactersToUse.charAt(randomBetween(0, charactersToUse.length - 1));
+			}
+		}
+	
+		return str.substr(0, str.length - 1);
 	}
-
+	
 	// Clamp value to range <a, b>
-	public static inline function clamp( x:Float, a:Float, b:Float ):Float{
-		return ( x < a ) ? a : ( ( x > b ) ? b : x );
+	public static function clamp(x:Float, a:Float, b:Float):Float {
+		return (x < a) ? a : ((x > b) ? b : x);
 	}
-
-	// Clamp value to range <a, inf)
-	public static inline function clampBottom( x:Float, a:Float ):Float{
+	
+	// Clamp value to range <a, inf>
+	public static function clampBottom(x:Float, a:Float):Float {
 		return x < a ? a : x;
 	}
-
+	
 	// Linear mapping from range <a1, a2> to range <b1, b2>
-	public static inline function mapLinear( x:Float, a1:Float, a2:Float, b1:Float, b2:Float ):Float{
-		return b1 + ( x - a1 ) * ( b2 - b1 ) / ( a2 - a1 );
+	public static function mapLinear(x:Float, a1:Float, a2:Float, b1:Float, b2:Float):Float {
+		return b1 + (x - a1) * (b2 - b1) / (a2 - a1);
 	}
-
+	
 	// http://en.wikipedia.org/wiki/Smoothstep
-	public static inline function smoothstep( x:Float, min:Float, max:Float ):Float{
-		if ( x <= min ) return 0;
-		if ( x >= max ) return 1;
+	public static function smoothstep(x:Float, min:Float, max:Float):Float {
+		if (x <= min) return 0;
+		if (x >= max) return 1;
 
-		x = ( x - min )/( max - min );
+		x = (x - min) / (max - min);
 
-		return x*x*(3 - 2*x);
+		return x * x * (3 - 2 * x);
 	}
+	
+	public static function smootherstep(x:Float, min:Float, max:Float):Float {
+		if (x <= min) return 0;
+		if (x >= max) return 1;
 
-	public static inline function smootherstep( x:Float, min:Float, max:Float ):Float{
-		if ( x <= min ) return 0;
-		if ( x >= max ) return 1;
+		x = (x - min) / (max - min);
 
-		x = ( x - min )/( max - min );
-
-		return x*x*x*(x*(x*6 - 15) + 10);
+		return x * x * x * (x * (x * 6 - 15) + 10);
 	}
-
+	
 	// Random float from <0, 1> with 16 bits of randomness
 	// (standard Math.random() creates repetitive patterns when applied over larger space)
-	public static inline function random16() {
-		return ( 65280 * Math.random() + 255 * Math.random() ) / 65535;
+	public static function random16():Float {
+		return (65280 * Math.random() + 255 * Math.random()) / 65535;
 	}
-
+	
 	// Random integer from <low, high> interval
-	public static inline function randInt( low:Float, high:Float ):Float{
-		return low + Math.floor( Math.random() * ( high - low + 1 ) );
+	public static function randInt(low:Int, high:Int):Int {
+		return Std.int(low + Math.floor(Math.random() * (high - low + 1)));
 	}
-
+	
 	// Random float from <low, high> interval
-	public static inline function randFloat( low:Float, high:Float ):Float{
-		return low + Math.random() * ( high - low );
+	public static function randFloat(low:Float, high:Float):Float {
+		return low + Math.random() * (high - low);
 	}
-
+	
 	// Random float from <-range/2, range/2> interval
-	public static inline function randFloatSpread( range:Float ):Float{
-		return range * ( 0.5 - Math.random() );
+	public static function randFloatSpread(range:Float):Float {
+		return range * (0.5 - Math.random());
 	}
-
-	public static inline function sign( x:Float ):Float{
-		return ( x < 0 ) ? - 1 : ( x > 0 ) ? 1 : 0;
+	
+	public static function sign(x:Float):Int {
+		return (x < 0) ? -1 : ((x > 0) ? 1 : 0);
 	}
-
-	static var degreeToRadiansFactor:Float = Math.PI / 180;
-	public static inline function degToRad(degrees:Float):Float{
+	
+	public static function degToRad(degrees:Float):Float {
 		return degrees * degreeToRadiansFactor;
 	}
-
-	static var radianToDegreesFactor:Float = 180 / Math.PI;
-	public static inline function radToDeg(radians:Float):Float{
+	
+	public static function radToDeg(radians:Float):Float {
 		return radians * radianToDegreesFactor;
 	}
 
-	public static inline function isPowerOfTwo( value:Int ):Bool{
-		return ( value & ( value - 1 ) ) == 0 && value != 0;
+	public static function isPowerOfTwo(value:Int):Bool {
+		return  ( value & (value - 1 )) == 0 && value != 0;
 	}
 }
